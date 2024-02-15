ba34716f5ec46dd0cab434cc678b3e5b2f9fc474
Dylan Ballback <Dylanballback19@gmail.com>
2024-02-15 00:36:01 -0500

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pyodrivecan
 import asyncio
 from datetime import datetime, timedelta
-import time
+
 
 #Example of how you can create a controller to get data from the O-Drives and then send motor comands based on that data.
 async def controller(odrive1, odrive2, odrive3):
@@ -9,22 +9,19 @@ async def controller(odrive1, odrive2, odrive3):
         print("Set odrive to postion 0")
         #odrive2.set_position(0)
         #odrive3.set_position(0)
-        time.sleep(3)
-        odrive1.set_position(1)
-        print("Set odrive to postion 1")
-        time.sleep(3)
-        odrive1.set_position(0)
-        print("Set odrive to postion 0")
-
+        
 
         #Run for set time delay example runs for 15 seconds.
         stop_at = datetime.now() + timedelta(seconds=15)
         while datetime.now() < stop_at:
             await asyncio.sleep(0) #Need this for async to work.
-            print(odrive1.position)
-            #odrive1.set_position(1)
-            #print("Set odrive to postion 1")
-
+            #print(odrive1.position)
+            odrive1.set_position(0)
+            print("Set odrive to postion 0")
+            await asyncio.sleep(3)
+            odrive1.set_position(1)
+            print("Set odrive to postion 1")
+            await asyncio.sleep(3)
             
     
             

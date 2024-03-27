47ef29e0309c1191bab459676b37f0ba7b8a56ec
Dylan Ballback <Dylanballback19@gmail.com>
2024-03-27 19:27:50 -0400

    clean up with functions

--- full_dog/stand.py
+++ full_dog/stand.py
@@ -11,6 +11,86 @@ back_left = [8, 7, 6]
 back_right = [9, 10, 11]
 
 
+front_left_knee = pyodrivecan.ODriveCAN(0)
+front_left_knee.initCanBus()
+
+front_left_shoulder = pyodrivecan.ODriveCAN(1)
+front_left_shoulder.initCanBus()
+    
+front_left_hip = pyodrivecan.ODriveCAN(2)
+front_left_hip.initCanBus()
+    
+
+#Front Right 
+front_right_knee = pyodrivecan.ODriveCAN(5)
+front_right_knee.initCanBus()
+    
+front_right_shoulder = pyodrivecan.ODriveCAN(4)
+front_right_shoulder.initCanBus()
+
+front_right_hip = pyodrivecan.ODriveCAN(3)
+front_right_hip.initCanBus()
+
+
+#Back of Dog
+
+#Back Left 
+back_left_knee = pyodrivecan.ODriveCAN(6, canBusID="can1")
+back_left_knee.initCanBus()
+    
+back_left_shoulder = pyodrivecan.ODriveCAN(7, canBusID="can1")
+back_left_shoulder.initCanBus()
+    
+back_left_hip = pyodrivecan.ODriveCAN(8, canBusID="can1")
+back_left_hip.initCanBus()
+    
+#Back Right 
+back_right_knee = pyodrivecan.ODriveCAN(11, canBusID="can1")
+back_right_knee.initCanBus() 
+    
+back_right_shoulder = pyodrivecan.ODriveCAN(10, canBusID="can1")
+back_right_shoulder.initCanBus()
+
+back_right_hip = pyodrivecan.ODriveCAN(9, canBusID="can1")
+back_right_hip.initCanBus()
+
+
+def clear_errors():
+    front_left_knee.clear_errors(identify=False)
+    front_left_shoulder.clear_errors(identify=False)
+    front_left_hip.clear_errors(identify=False)
+
+    front_right_knee.clear_errors(identify=False)
+    front_right_shoulder.clear_errors(identify=False)
+    front_right_hip.clear_errors(identify=False)
+
+    back_left_knee.clear_errors(identify=False)
+    back_left_shoulder.clear_errors(identify=False)
+    back_left_hip.clear_errors(identify=False)
+
+    back_right_knee.clear_errors(identify=False)
+    back_right_shoulder.clear_errors(identify=False)
+    back_right_hip.clear_errors(identify=False)
+
+def set_closed_loop():
+    front_left_knee.setAxisState("closed_loop_control")
+    front_left_shoulder.setAxisState("closed_loop_control")
+    front_left_hip.setAxisState("closed_loop_control")
+
+    front_right_knee.setAxisState("closed_loop_control")
+    front_right_shoulder.setAxisState("closed_loop_control")
+    front_right_hip.setAxisState("closed_loop_control")
+
+    back_left_knee.setAxisState("closed_loop_control")
+    back_left_shoulder.setAxisState("closed_loop_control")
+    back_left_hip.setAxisState("closed_loop_control")
+
+    back_right_knee.setAxisState("closed_loop_control")
+    back_right_shoulder.setAxisState("closed_loop_control")
+    back_right_hip.setAxisState("closed_loop_control")
+
+        
+
 
 #Example of how you can create a controller to get data from the O-Drives and then send motor comands based on that data.
 async def controller(front_left_knee, front_left_shoulder, front_left_hip,
@@ -18,68 +98,18 @@ async def controller(front_left_knee, front_left_shoulder, front_left_hip,
                     back_left_knee, back_left_shoulder, back_left_hip,
                     back_right_knee, back_right_shoulder, back_right_hip):
         
-        sleep_time = 6
+        clear_errors()
         
-        front_left_knee.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-        front_left_shoulder.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-        front_left_hip.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-
-        front_right_knee.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-        front_right_shoulder.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-        front_right_hip.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-
-        back_left_knee.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-        back_left_shoulder.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-        back_left_hip.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-
-        back_right_knee.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-        back_right_shoulder.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
-        back_right_hip.clear_errors(identify=True)
-        await asyncio.sleep(sleep_time)
+        set_closed_loop()
         
 
-        front_left_knee.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        front_left_shoulder.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        front_left_hip.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        front_right_knee.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        front_right_shoulder.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        front_right_hip.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        back_left_knee.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        back_left_shoulder.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        back_left_hip.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        back_right_knee.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        back_right_shoulder.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        back_right_hip.setAxisState("closed_loop_control")
-        await asyncio.sleep(sleep_time)
-        
-
+        print("Standing")
         #Run for set time delay example runs for 15 seconds.
         stop_at = datetime.now() + timedelta(seconds=15)
         while datetime.now() < stop_at:
             await asyncio.sleep(0) #Need this for async to work.
-            print("Standing")
+            pass
+        
             #odrive1.set_position(0)
             #print("Set odrive to postion 0")
             #await asyncio.sleep(3)
@@ -89,65 +119,16 @@ async def controller(front_left_knee, front_left_shoulder, front_left_hip,
             
 
 
+    
+
+
 
 # Run multiple busses.
 async def main():
-    sleep_time = 5
+
     #Front of Dog
          
-    front_left_knee = pyodrivecan.ODriveCAN(0)
-    front_left_knee.initCanBus()
-    time.sleep(sleep_time)
-    #front_left_knee.clear_errors(identify=False)
-
-    front_left_shoulder = pyodrivecan.ODriveCAN(1)
-    front_left_shoulder.initCanBus()
-    time.sleep(sleep_time)
-
-    front_left_hip = pyodrivecan.ODriveCAN(2)
-    front_left_hip.initCanBus()
-    time.sleep(sleep_time)
-
-    #Front Right 
-    front_right_knee = pyodrivecan.ODriveCAN(5)
-    front_right_knee.initCanBus()
-    time.sleep(sleep_time)
-
-    front_right_shoulder = pyodrivecan.ODriveCAN(4)
-    front_right_shoulder.initCanBus()
-    time.sleep(sleep_time)
-
-    front_right_hip = pyodrivecan.ODriveCAN(3)
-    front_right_hip.initCanBus()
-    time.sleep(sleep_time)
-    #Back of Dog
-
-    #Back Left 
-    back_left_knee = pyodrivecan.ODriveCAN(6, canBusID="can1")
-    back_left_knee.initCanBus()
-    time.sleep(sleep_time)
-
-    back_left_shoulder = pyodrivecan.ODriveCAN(7, canBusID="can1")
-    back_left_shoulder.initCanBus()
-    time.sleep(sleep_time)
-
-    back_left_hip = pyodrivecan.ODriveCAN(8, canBusID="can1")
-    back_left_hip.initCanBus()
-    time.sleep(sleep_time)
     
-    #Back Right 
-    back_right_knee = pyodrivecan.ODriveCAN(11, canBusID="can1")
-    back_right_knee.initCanBus()
-    time.sleep(sleep_time)
-    
-    back_right_shoulder = pyodrivecan.ODriveCAN(10, canBusID="can1")
-    back_right_shoulder.initCanBus()
-    time.sleep(sleep_time)
-
-    back_right_hip = pyodrivecan.ODriveCAN(9, canBusID="can1")
-    back_right_hip.initCanBus()
-    time.sleep(sleep_time)
-
     try:
         await asyncio.gather(
             front_left_knee.loop(),

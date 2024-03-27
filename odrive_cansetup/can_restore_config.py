d4cb35ead904e5d1c575cfa22c16224b78b2da4b
Dylan Ballback <Dylanballback19@gmail.com>
2024-03-27 19:12:45 -0400

    update can_restore config to update all openmutts odrives

--- odrive_cansetup/can_restore_config.py
+++ odrive_cansetup/can_restore_config.py
@@ -91,13 +91,19 @@ async def restore_config(odrv: EndpointAccess, config: dict):
 async def main():
     parser = argparse.ArgumentParser(description='Script to configure ODrive over CAN bus.')
     parser.add_argument('-i', '--interface', type=str, default='socketcan', help='Interface type (e.g., socketcan, slcan). Default is socketcan.')
-    parser.add_argument('-c', '--channel', type=str, required=True, help='Channel/path/interface name of the device (e.g., can0, /dev/tty.usbmodem11201).')
+    parser.add_argument('-c', '--channel', type=str, required=False, help='Channel/path/interface name of the device (e.g., can0, /dev/tty.usbmodem11201).')
     parser.add_argument('-b', '--bitrate', type=int, default=250000, help='Bitrate for CAN bus. Default is 250000.')
     parser.add_argument('--node-id', type=int, required=True, help='CAN Node ID of the ODrive.')
-    parser.add_argument('--endpoints-json', type=str, required=True, help='Path to flat_endpoints.json corresponding to the given ODrive and firmware version.')
-    parser.add_argument('--config', type=str, required=True, help='JSON file with configuration settings.')
+    parser.add_argument('--endpoints-json', default='flat_endpoints.json', type=str, required=False, help='Path to flat_endpoints.json corresponding to the given ODrive and firmware version.')
+    parser.add_argument('--config', type=str, default='config.json', required=False, help='JSON file with configuration settings.')
     parser.add_argument("--save-config", action='store_true', help="Save the configuration to NVM and reboot ODrive.")
     args = parser.parse_args()
+
+    #This is for OPENMutt front motors are on can0 while back motors are on can1
+    node_ids_channels = {
+        'can0': list(range(0, 6)),
+        'can1': list(range(6, 12))
+    }
 
     with open(args.endpoints_json, 'r') as f:
         endpoint_data = json.load(f)
@@ -105,22 +111,23 @@ async def main():
     with open(args.config, 'r') as f:
         config_list = json.load(f)
 
-    print("opening CAN bus...")
-    with can.interface.Bus(args.channel, bustype=args.interface, bitrate=args.bitrate) as bus:
-        #reader = can.AsyncBufferedReader()
-        #notifier = can.Notifier(bus, [reader], loop=asyncio.get_running_loop())
-        with CanSimpleNode(bus=bus, node_id=args.node_id) as node:
-            odrv = EndpointAccess(node=node, endpoint_data=endpoint_data)
+    for channel, node_ids in node_ids_channels.items():
+        print(f"Opening CAN bus on {channel}...")
+        with can.interface.Bus(channel, bustype=args.interface, bitrate=args.bitrate) as bus:
+            for node_id in node_ids:
+                print(f"Configuring node {node_id} on {channel}...")
+                with CanSimpleNode(bus=bus, node_id=node_id) as node:
+                    odrv = EndpointAccess(node=node, endpoint_data=endpoint_data)
 
-            print("checking version...")
-            await odrv.version_check()
-            await restore_config(odrv, config_list)
+                    print("Checking version...")
+                    await odrv.version_check()
+                    await restore_config(odrv, config_list)
 
-            if args.save_config:
-                print(f"saving configuration...")
-                node.reboot_msg(REBOOT_ACTION_SAVE)
+                    if args.save_config:
+                        print(f"Saving configuration for node {node_id}...")
+                        node.reboot_msg(REBOOT_ACTION_SAVE)
 
-        await asyncio.sleep(0.1) # needed for last message to get through on SLCAN backend
+                    await asyncio.sleep(0.1)  # small delay between configurations
 
 if __name__ == "__main__":
     asyncio.run(main())

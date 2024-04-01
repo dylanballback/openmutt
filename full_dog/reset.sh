3a8000df2f09e343cce882c589dc2e43efe6499a
Dylan Ballback <Dylanballback19@gmail.com>
2024-03-31 23:45:13 -0400

    can reset bash script

---
+++ full_dog/reset.sh
@@ -0,0 +1,14 @@
+sudo /sbin/ip link set can0 down
+sudo ip link set can0 type can restart-ms 100
+
+sudo /sbin/ip link set can1 down
+sudo ip link set can1 type can restart-ms 100
+#ip link set can0 type can restart
+
+#echo("Reset was Succesful")
+
+sudo ip link set can0 up type can bitrate 250000
+sudo ip link set can1 up type can bitrate 250000
+candump can0 -xct z -n 10
+candump can1 -xct z -n 10
+

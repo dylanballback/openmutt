6b5f23b646a4f6410228f4908701960627cde86a
Dylan Ballback <Dylanballback19@gmail.com>
2024-04-05 11:47:14 -0400

    update

--- full_dog/swing.py
+++ full_dog/swing.py
@@ -210,7 +210,7 @@ async def controller():
         await asyncio.sleep(2)
         
         
-        hip_position = 1.7
+        hip_position = 2.1
         front_right_hip.set_position(hip_position)
         front_left_hip.set_position(-hip_position)
         back_right_hip.set_position(-hip_position)

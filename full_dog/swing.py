e5672aa3183c4b9428f29258a50b9b9d9632a8ef
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2024-04-05 18:19:41 -0400

    calibrate

--- full_dog/swing.py
+++ full_dog/swing.py
@@ -364,13 +364,13 @@ async def controller():
         #await asyncio.sleep(0.2)
         
         # You must calibrate when the O-Drives are first powered up.
-        #await calibrate()
-        #await asyncio.sleep(10)
+        await calibrate()
+        await asyncio.sleep(10)
         
         
-        await set_all_filtered_pos_control()
+        #await set_all_filtered_pos_control()
         #await closedloop_lower()
-        await asyncio.sleep(2)
+        #await asyncio.sleep(2)
         
         
         """
@@ -419,7 +419,7 @@ async def controller():
 
         #await idle_lower()
 
-        stand()
+        #stand()
 
         # This is for setting a set of legs to idle and printing positions.
         #await asyncio.gather(front_idle_lower(), back_idle_lower(), print_positions_continuously(1000))
